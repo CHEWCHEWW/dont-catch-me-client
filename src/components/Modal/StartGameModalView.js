b700b9b9b5fb232e0b33944a18c5e27b458c4e07
Minjieun <chewchewchew3@gmail.com>
2021-05-12 00:30:05 +0900

    [ADD]: Start game modal template

---
+++ src/components/Modal/StartGameModalView.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+import ModalView from "../shared/ModalView";
+
+const StartGameModalView = ({ onClick }) => {
+  return (
+    <ModalView padding={20} width={700} height={500}>
+      <ModalContent onClick={onClick}>
+        <TextBoard>
+
+        </TextBoard>
+        <button onClick={onClick}>Start</button>
+      </ModalContent>
+    </ModalView>
+  );
+};
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  background: red;
+`;
+
+const TextBoard = styled.div`
+  width: 80%;
+  height: 40%;
+  background: red;
+`;
+
+export default StartGameModalView;
--- src/pages/GamePage/index.js
+++ src/pages/GamePage/index.js
@@ -1,14 +1,31 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
+import Modal from "../../components/Modal";
+import StartGameModalView from "../../components/Modal/StartGameModalView";
 import { config } from "../../phaser/gameObjects/Game";
 
 const GamePage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(true);
+  const [isGameStart, setIsGameStart] = useState(false);
+
   useEffect(() => {
-    const game = new Phaser.Game(config);
-  }, []);
+    if (isGameStart) {
+      const game = new Phaser.Game(config);
+    }
+  }, [isGameStart]);
+
+  const handleModalClick = () => {
+    setIsGameStart(true);
+    setIsModalOpen(false);
+  };
 
   return (
     <>
+      {isModalOpen && (
+        <Modal>
+          <StartGameModalView onClick={handleModalClick} />
+        </Modal>
+      )}
       <div>title</div>
       <div id="game-container" />
     </>
