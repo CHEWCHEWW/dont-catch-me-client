b5d1c6e9fe48339c818c2d80aadcbf9bee5b755a
Minjieun <chewchewchew3@gmail.com>
2021-05-11 22:55:41 +0900

    [ADD]: Modal component

---
+++ src/components/Modal/index.js
@@ -0,0 +1,46 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+
+const Modal = ({ children, onBackgroundClick }) => {
+  useEffect(() => {
+    document.body.classList.add("hidden");
+
+    return () => {
+      document.body.classList.remove("hidden");
+    };
+  }, []);
+
+  return (
+    <ModalWrapper>
+      <ModalBackground onClick={onBackgroundClick} />
+      <ModalContent>
+        {children}
+      </ModalContent>
+    </ModalWrapper>
+  );
+};
+
+const ModalBackground = styled.div`
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background: ${({ theme }) => theme.ModalBackground};
+  z-index: 1;
+  overflow: hidden;
+`;
+
+const ModalContent = styled.div`  
+  display: block;
+  position: fixed;
+  overflow-x: hidden;
+  overflow-y: auto;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+`;
+
+export default Modal;
