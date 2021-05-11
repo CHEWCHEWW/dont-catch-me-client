8d0a4d7dc053344a609e646fbeea46f12cb78535
Minjieun <chewchewchew3@gmail.com>
2021-05-11 17:32:57 +0900

    [FIX]: Wrong Indentation of direction util

--- src/utils/directions.js
+++ src/utils/directions.js
@@ -1,22 +1,22 @@
 export const getOrderedDirections = (filter) => {
-	const directions = [Direction.Up, Direction.Left, Direction.Down, Direction.Right];
-	
-  if (!filter) {
-		return directions;
-	}
+  const directions = [Direction.Up, Direction.Left, Direction.Down, Direction.Right];
 
-	return directions.filter(filter);
+  if (!filter) {
+    return directions;
+  }
+
+  return directions.filter(filter);
 };
 
 export const getOppositeDirection = (direction) => {
-	switch (direction) {
-		case Direction.Left:
-			return Direction.Right
-		case Direction.Right:
-			return Direction.Left
-		case Direction.Up:
-			return Direction.Down
-		case Direction.Down:
-			return Direction.Up
-	}
+switch (direction) {
+  case Direction.Left:
+    return Direction.Right;
+  case Direction.Right:
+    return Direction.Left;
+  case Direction.Up:
+    return Direction.Down;
+  case Direction.Down:
+    return Direction.Up;
+  }
 };
