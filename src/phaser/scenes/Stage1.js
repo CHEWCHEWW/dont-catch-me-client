8c6733f0e5bec10a6a376e54550b961e48b74c69
Minjieun <chewchewchew3@gmail.com>
2021-05-13 09:55:57 +0900

    [ADD]: Coins to map

--- src/phaser/scenes/Stage1.js
+++ src/phaser/scenes/Stage1.js
@@ -10,13 +10,14 @@ export default class Stage1 extends Phaser.Scene {
   constructor() {
     super("stage1");
   }
-  
+
   create() {
     this.map = this.add.tilemap("map");
 
     const tileset = this.map.addTilesetImage("iso-level1", "tiles");
 
     this.boardLayer = this.map.createLayer("Tile Layer 1", [tileset]);
+    this.coinLayer = this.map.createLayer("Tile Layer 2", [tileset]);
 
     this.hero = new Hero(this, 300, 300, "hero");
     this.enemy = new Enemy(this, 200, 200, "hero");
@@ -40,32 +41,45 @@ export default class Stage1 extends Phaser.Scene {
 		this.cameras.main.startFollow(this.hero, true);
     this.cameras.main.setZoom(0.8);
 
-    this.timer = this.time.delayedCall(30000, this.gameOver, [], this);
+    // this.timer = this.time.delayedCall(10000, this.levelUp, [], this);
     this.countDown = this.add.text(32, 32);
 
+    const coins = this.coinLayer.createFromTiles(6, -1, { key: "coin" });
+
+    coins.forEach((coin) => {
+			this.physics.add.existing(coin);
+      console.log(coin);
+			const body = coin.body;
+
+			body.setCircle(4, 12, 12);
+		});
+    
     this.physics.add.collider([this.enemy, this.enemy1]);
 
     this.physics.add.collider(this.hero, [this.enemy, this.enemy1], () => {
       this.gameOver();
     });
-
   }
 
   update(time, delta) {
     if (this.hero) {
-      this.hero.handleMovement(delta, this.cursors, this.boardLayer);
+      this.hero.handleMovement(delta, this.cursors, this.boardLayer, this.coinLayer);
     }
 
-    this.countDown.setText("current" + this.timer.getProgress().toString().substr(0, 4));
+    // this.countDown.setText("current" + this.timer.getProgress().toString().substr(0, 4));
   }
 
   gameOver() {
-    this.time.addEvent({
-      callback: () => {
-        this.scene.pause();
-        store.dispatch(updateGameProgress(gameProgress.GAME_OVER));
-      },
-      delay: 1000,
-    });
+    // this.time.addEvent({
+    //   callback: () => {
+    //     this.scene.pause();
+    //     store.dispatch(updateGameProgress(gameProgress.GAME_OVER));
+    //   },
+    //   delay: 1000,
+    // });
+  }
+
+  levelUp() {
+    this.scene.start("stage2");
   }
 }
