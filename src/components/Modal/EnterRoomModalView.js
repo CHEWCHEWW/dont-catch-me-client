55e40b6ff7a7771717977240ad4ec633c13c9fdf
Minjieun <chewchewchew3@gmail.com>
2021-05-15 08:08:50 +0900

    [FIX]: Enter room modal view

--- src/components/Modal/EnterRoomModalView.js
+++ src/components/Modal/EnterRoomModalView.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 
+import { enterRoom } from "../../redux/slices/roomSlice";
 import ModalView from "../shared/ModalView";
 import ModalContent from "../shared/ModalContent";
 
-const EnterRoomModalView = ({ userName, onChange, onSubmit }) => {
+
+const EnterRoomModalView = () => {
+  const dispatch = useDispatch();
+  const [invitationCode, setInvitationCode] = useState("");
+
+  const handleInvitationCodeChange = ({ target: { value } }) => {
+    setInvitationCode(value);
+  };
+
+  const handleInvitationCodeSubmit = (ev) => {
+    ev.preventDefault();
+
+    dispatch(enterRoom({ roomID: invitationCode }));
+  };
+
   return (
     <ModalView padding={20} width={400} height={300}>
       <ModalContent>
-        <ModalForm onSubmit={onSubmit}>
-          <input type="text" name="userName" value={userName} onChange={onChange} />
+        <ModalForm onSubmit={handleInvitationCodeSubmit}>
+          <input type="text" name="invitiaionCode" value={invitationCode} onChange={handleInvitationCodeChange} />
           <input type="submit" value="Join" />
         </ModalForm>
       </ModalContent>
