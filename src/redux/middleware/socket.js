1d10bed145da98899607837f164bdd14326e594b
Minjieun <chewchewchew3@gmail.com>
2021-05-14 14:44:48 +0900

    [ADD]: Connect with socket server

--- src/redux/middleware/socket.js
+++ src/redux/middleware/socket.js
@@ -1,11 +1,15 @@
 import { io } from "socket.io-client";
 
+import { sendSocketMessage } from "../slices/gameSlice";
+
 const socketMiddleware = (url) => {
+  console.log(300);
   return (store) => {
     const socket = io.connect(url);
-
-    socket.on("message", (message) => {
-      console.log(30);
+    socket.emit("test", { message: "24" });
+    socket.on("success", (message) => {
+      console.log(message);
+      store.dispatch(sendSocketMessage("happy"));
     });
 
     return (next) => (action) => {
