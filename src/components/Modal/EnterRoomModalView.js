c61982135a04575b6b42bbdbd282260046543c96
Minjieun <chewchewchew3@gmail.com>
2021-05-15 14:49:43 +0900

    [DELETE]: Unneeded start button

--- src/components/Modal/EnterRoomModalView.js
+++ src/components/Modal/EnterRoomModalView.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
