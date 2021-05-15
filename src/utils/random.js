bff057a10b31ee5295eb10d3f7a1ffc8529cb8bf
Minjieun <chewchewchew3@gmail.com>
2021-05-15 09:21:17 +0900

    [ADD]: Connect create room with socket

--- src/utils/random.js
+++ src/utils/random.js
@@ -2,4 +2,8 @@ export const shuffleOrder = (array) => {
   return array.sort(() => Math.random() - 0.5);
 };
 
+export const generateRandomCode = () => {
+  return Math.random().toString(36).substring(7);
+};
+
 export const generateRandomEnemy = () => {};
