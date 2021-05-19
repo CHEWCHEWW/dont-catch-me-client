741e28a092610aaf3e21c6215659fef5be549215
Minjieun <chewchewchew3@gmail.com>
2021-05-19 09:04:08 +0900

    [ADD]: Rotate AI

--- src/utils/directions.js
+++ src/utils/directions.js
@@ -21,6 +21,8 @@ export const getSideDirection = (direction) => {
       return Direction.Right;
     case Direction.Down:
       return Direction.Right;
+    default:
+      break;
   }
 };
 
@@ -42,4 +44,84 @@ export const getPositionByDirection = (x, y, direction) => {
       break;
     }
   }
+};
+
+export const determineRotationDirection = ({ 
+  currentX, 
+  currentY, 
+  lastDirection, 
+  directions, 
+  board 
+}) => { 
+  const { x: directionX, y: directionY } = getPositionByDirection(currentX, currentY, lastDirection);
+  
+  const currentDirectionDistnace = Phaser.Math.Distance.Between(
+    directionX,
+    directionY,
+    currentX,
+    currentY,
+  );
+    console.log(currentDirectionDistnace);
+  if (board.getTileAtWorldXY(directionX + 12, directionY + 60) && currentDirectionDistnace < 350) {
+    return lastDirection;
+  }
+
+  for (const direction of directions) {
+    const { x: positionX, y: positionY } = getPositionByDirection(currentX, currentY, direction);
+    
+    if (!board.getTileAtWorldXY(positionX + 12, positionY + 60)) {
+      continue;
+    }
+
+    const distance = Phaser.Math.Distance.Between(
+      positionX,
+      positionY,
+      currentX,
+      currentY
+    );
+    
+    if (distance < 350 || direction === Direction.None) {
+      return direction;
+    }
+  }
+
+  return false;
+};
+
+export const determineDirectionByTarget = ({
+  targetX, 
+  targetY, 
+  currentX, 
+  currentY, 
+  directions, 
+  board, 
+  oppositeDirection,
+}) => {
+  let lastClosedDirection = Direction.None;
+  let lastClosedDistance = -1;
+  
+  for (const direction of directions) {
+    const { x: positionX, y: positionY } = getPositionByDirection(currentX, currentY, direction);
+    
+    if (!board.getTileAtWorldXY(positionX + 12, positionY + 60)) {
+      continue;
+    }
+
+    const distance = Phaser.Math.Distance.Between(
+      positionX,
+      positionY,
+      targetX,
+      targetY
+    );
+    
+    if (
+      lastClosedDirection === Direction.None ||
+      distance < lastClosedDistance
+    ) {
+      lastClosedDirection = direction;
+      lastClosedDistance = distance;
+    }
+  }
+
+  return lastClosedDirection !== Direction.None ? lastClosedDirection : oppositeDirection;
 };
