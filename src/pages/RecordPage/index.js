dc859d2edb207403c6bbe146538193af1b9251a9
Minjieun <chewchewchew3@gmail.com>
2021-05-20 22:53:00 +0900

    [ADD]: Record page styling

--- src/pages/RecordPage/index.js
+++ src/pages/RecordPage/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 
 import { getGameRecords } from "../../redux/slices/singlePlaySlice";
@@ -9,30 +10,40 @@ import MainPageLayout from "../../components/MainPageLayout";
 const RecordPage = () => {
   const dispatch = useDispatch();
   const records = useSelector(({ single }) => single.records);
+  const history = useHistory();
 
   useEffect(() => {
     dispatch(getGameRecords());
   }, []);
 
+  const handleBackButtonClick = () => {
+    history.push("/");
+  };
+
   return (
     <MainPageLayout>
-      <PageCard width={550} height={600}>
-        <CardList>
+      <PageCard width={550} height={630}>
+        <Content>
           <Title>Best Top 7</Title>
           {records.map((record, index) => (
-            <Card key={index}>
-              <Ranking>{index + 1}</Ranking>
-              <Content>{record.username}</Content>
-              <Content>{record.score}</Content>
+            <Card key={index} isTop={index < 3}>
+              <Ranking>
+                {index + 1}
+              </Ranking>
+              <div>{record.username}</div>
+              <div>{record.score}</div>
             </Card>
           ))}
-        </CardList>
+          <Button onClick={handleBackButtonClick}>
+            BACK
+          </Button>
+        </Content>
       </PageCard>
     </MainPageLayout>
   );
 };
 
-const CardList = styled.div`
+const Content = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
@@ -54,11 +65,25 @@ const Card = styled.div`
   box-shadow: 0 1.5px 1.5px rgba(0, 0, 0, 0.2);
 
   :hover {
-    background: #F6F0EB;
+    background: #F9F1EA;
   }
 `;
 
-const Content = styled.div`
+const Button = styled.button`
+  width: 100%;
+  height: 6%;
+  border-radius: 0 0 5px 5px;
+  margin-top: 20px;
+  background: #e55643;
+  color: white;
+  border: 0;
+  font-size: 20px;
+  text-shadow: 0 -2px 2px rgba(175, 49, 95, 0.9);
+
+  :hover {
+    background-color: #F16A58;
+    text-shadow: 0 -1px 1px rgba(175, 49, 95, 0.9), 0 0 5px rgba(255, 255, 255, 0.8);
+  }
 `;
 
 const Ranking = styled.h3`
