3070b07e5c37a25d8e2c936adb0a2e5a782f55c0
Minjieun <chewchewchew3@gmail.com>
2021-05-22 08:18:47 +0900

    [DELETE]: Unneeded socket option

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,6 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io.connect(process.env.SERVER_PORT, {
-  transports: ["websocket"],
-  withCredentials: true,
-});
+export const socket = io.connect(process.env.SERVER_PORT);
